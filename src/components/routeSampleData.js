ff9a579f33e186a07ccd908978aab05924b1e8ac
Theresa <tli2@cca.com>
2019-05-18 16:54:44 -0700

    Set up mock data for routes

---
+++ src/components/routeSampleData.js
@@ -0,0 +1,16 @@
+const routeSampleData = [
+  {
+    routeName: "AngelHack",
+    rating: 5,
+    completionRate: 100,
+    stage: 0
+  },
+  {
+    routeName: "TagVenture",
+    rating: 4.5,
+    completionRate: 51,
+    stage: 3
+  }
+];
+
+export default routeSampleData;
