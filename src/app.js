6c33e01acda3cea69f0ec3a441c141656f4ab38f
Linden Chiu <hchiu87@gmail.com>
2019-05-18 23:05:32 -0700

    working multi-component

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import './index.css';
-import config from './aws-exports';
+//import config from './aws-exports';
 import Amplify from 'aws-amplify';
 
-Amplify.configure(config)
+//Amplify.configure(config)
 
 ReactDOM.render(<App />, document.getElementById('app'));
